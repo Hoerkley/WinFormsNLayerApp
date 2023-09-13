cdd153f422e8ad277f493a0d3051bb656b12d6be
Senai <Senai@CEPEAS21103947>
2023-09-12 21:54:53 -0300

    Implementando repositorio de cargos.

---
+++ iMyApp/Negocio/Negocio/Entidade/Cargo.cs
@@ -0,0 +1,16 @@
+﻿using Negocio.Comum;
+using Negocio.Entidade.Comum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Entidade
+{
+    public class Cargo : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public bool Status { get; set;}
+    }
+}
