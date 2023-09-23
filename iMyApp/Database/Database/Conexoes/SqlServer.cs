67a7850c20a8e1fbfd5a5eaf96f7cca27b0660e1
Senai <Senai@CEPEAS21103947>
2023-09-23 11:35:07 -0300

    Aplicação web

--- iMyApp/Database/Database/Conexoes/SqlServer.cs
+++ iMyApp/Database/Database/Conexoes/SqlServer.cs
@@ -18,7 +18,7 @@ namespace Database.Conexoes
         /// <returns> string: ConnectionString</returns>
         internal static string StrConexao() 
         {
-            return @"Data Source=.\SQLEXPRESS;Initial Catalog=IMyApp;User ID=sa;Password=sql2022;Trusted_Connection=False; TrustServerCertificate=True;"; 
+            return @"Data Source=.\SQLEXPRESS;Initial Catalog=NorthWind;User ID=sa;Password=sql2022;Trusted_Connection=False; TrustServerCertificate=True;"; 
         }
     }
 }
---
+++ iMyApp/Negocio/Negocio/Entidade/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Entidade
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set;}
+        public int Estoque { get; set;}
+        public string Categoria { get; set;}
+
+
+    }
+}
